5798a8daa0ce6306bd6e624ac4877654d60f78ef
RRO (RSL-Md. Motiur Rahman) <RRO@rahimafrooz.com>
2022-10-30 17:45:18 +0600

    update code

---
+++ index.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+app.use(cors());
+app.use(express.json());
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const port = process.env.PORT || 8000;
+
+// Password: Uf25rZozfSY9AvhU
+// user: morahman
+
+const uri =
+  "mongodb+srv://morahman:Uf25rZozfSY9AvhU@hero-one.z3ku6ig.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    const userCollection = client.db("simpleNode").collection("user");
+
+    //Get Data
+    app.get("/users", async (req, res) => {
+      const cursor = userCollection.find({});
+      const users = await cursor.toArray();
+      res.send(users);
+    });
+
+    // Post Data
+    app.post("/users", async (req, res) => {
+      const userData = req.body;
+      const result = await userCollection.insertOne(userData);
+      userData.id = result.id;
+      console.log(result);
+      res.send(userData);
+    });
+
+    // Update Data
+    app.put("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const userData = req.body;
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          name: userData.name,
+          email: userData.name,
+        },
+      };
+
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      res.send(result);
+    });
+
+    // Delete Data
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      if (result.deletedCount > 0) {
+        console.log("Successfully deleted one document.");
+      } else {
+        console.log("No documents matched the query. Deleted 0 documents.");
+      }
+    });
+  } finally {
+    //await client.close();
+  }
+}
+
+run().catch((err) => console.log(error));
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
